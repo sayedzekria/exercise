12b660a7c5af8319591d3a6396db16965da2b5e0
sayed zekria <sayed.zekria.sayedi@tekschool.us>
2024-01-31 13:35:01 -0600

    this is exercise

---
+++ src/main/java/practiceOne.java
@@ -0,0 +1,7 @@
+public class practiceOne {
+    public static void main(String[] args) {
+        System.out.println("codding is love");
+        System.out.println("java is my faivorate codding class");
+        System.out.println("codding the best interment");
+    }
+}
